8d32b96a69f8e22354be4feeceffacde0843e108
Nathan <nathanrbourke@gmail.com>
2016-03-06 11:44:01 -0500

    create basic user profile, hit badges API for user badge display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
+require 'httparty'
+
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @badges = HTTParty
+      .get("http://api.rolledaces.com/apiv1/student/#{params[:id]}")["badge_set"]
   end
 end
